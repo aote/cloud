026b73e2a6230b70ca748435c5f359c69bb8cf50
Weicf <332780922@qq.com>
2020-05-27 00:48:56 +0800

    配置eureka.

--- cloud-eureka-server-7001/src/main/java/com/aote/springcloud/EurekaMain7001.java
+++ cloud-eureka-server-7001/src/main/java/com/aote/springcloud/EurekaMain7001.java
@@ -2,20 +2,20 @@ package com.aote.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author aote
- * @Date 2020-05-26 23:43
+ * @Date 2020-05-27 00:35
  * @Version 1.0
  * @Description TODO
  **/
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 
 }
