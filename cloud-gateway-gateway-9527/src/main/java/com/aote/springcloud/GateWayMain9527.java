f4a521b98d4faa37472a90cf71ce4d03b4cb365b
wcf@blibao.cn <19821122>
2020-06-01 22:23:42 +0800

    gateway

---
+++ cloud-gateway-gateway-9527/src/main/java/com/aote/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.aote.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: Weicf
+ * @date: 2020-06-01 21:19
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+
+}
